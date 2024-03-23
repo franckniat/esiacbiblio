bd66b2f6c0349a9f0ab93f305856d7efd85c37f1
franckniat <niatmanuel2002@gmail.com>
2024-03-23 12:59:50 +0100

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Space_Grotesk } from "next/font/google";
+import "./globals.css";
+
+const space_grotesk = Space_Grotesk({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "ESIAC-BIBLIO",
+  description: "Application de la communauté des étudiants de l'ESIAC.",
+  applicationName: "ESIAC-BIBLIO",
+  keywords: ["Etudiants camerounais","ESIAC", "BIBLIO", "ESIAC-BIBLIO","Ecole Supérieure d'ingénierie et de management d'Afrique Centrale"],
+  creator: "Franck NIAT",
+  authors: [{
+    name: "Franck NIAT",
+    url: "https://franckinato.vercel.app",
+  }],
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="fr">
+      <body className={`${space_grotesk.className} min-h-screen antialiased`}>{children}</body>
+    </html>
+  );
+}
