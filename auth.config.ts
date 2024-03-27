c8473f6ae8ec86296d65654001092c8ff507d3bb
franckniat <niatmanuel2002@gmail.com>
2024-03-27 16:54:34 +0100

    (update) customize callback functions for authentication

--- auth.config.ts
+++ auth.config.ts
@@ -7,28 +7,6 @@ import Google from "next-auth/providers/google";
 import { LoginSchema } from './schemas';
 
 export const authConfig = {
-  /*pages: {
-    signIn: '/auth/login',
-  },
-   callbacks: {
-    async session({token, session}){
-      if(token.sub && session.user){
-        session.user.id = token.sub;
-      }
-      if(token.role && session.user){
-        session.user.role = token.role as UserRole;
-      }
-      return session
-    },
-
-    async jwt({token}){
-      if(!token.sub) return token;
-      const existingUser = await getUserById(token.sub);
-      if(!existingUser) return token;
-      token.role = existingUser.role;
-      return token;
-    }
-  }, */
   providers: [
     Credentials({
       async authorize(credentials){
--- auth.ts
+++ auth.ts
@@ -3,11 +3,40 @@ import { authConfig } from '@/auth.config';
 
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import { db } from '@/lib/db';
+import { UserRole } from '@prisma/client';
+import { getUserById } from '@/data/user';
 
 export const { 
     auth, signIn, signOut, 
     handlers:{GET, POST} 
 } = NextAuth({
+    callbacks:{
+        //TODO: Implement email verification
+        /* async signIn({user}){
+            const existingUser = await getUserById(user.id);
+            if(!existingUser || !existingUser.emailVerified) {
+                return false;
+            }
+            return true;
+        }, */
+        async session({token, session}){
+            if(token.sub && session.user){
+              session.user.id = token.sub;
+            }
+            if(token.role && session.user){
+              session.user.role = token.role as UserRole;
+            }
+            return session
+          },
+      
+          async jwt({token}){
+            if(!token.sub) return token;
+            const existingUser = await getUserById(token.sub);
+            if(!existingUser) return token;
+            token.role = existingUser.role;
+            return token;
+          }
+    },
     adapter: PrismaAdapter(db),
     session: {strategy: "jwt"},
     ...authConfig,
