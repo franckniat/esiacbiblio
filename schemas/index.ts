b74ec95675930d12af8be4bff2942f386e387ad1
franckniat <niatmanuel2002@gmail.com>
2024-03-29 02:35:50 +0100

    (add) setup document schema

--- schemas/index.ts
+++ schemas/index.ts
@@ -20,3 +20,24 @@ export const RegisterSchema = z.object({
         message: "Votre mot de passe doit avoir minimum 6 caractères ! "
     })
 })
+
+export const DocumentsSchema = z.object({
+    sector: z.string().min(1,{
+        message:"Veuillez choisir une filière !"
+    }),
+    category: z.string().min(1,{
+        message:"Veuillez choisir une catégorie !"
+    }),
+    title: z.string().min(1,{
+        message:"Veuillez entrer un titre !"
+    }),
+    description: z.string().min(1,{
+        message:"Veuillez entrer une description !"
+    }),
+    fileURL: z.string().min(1,{
+        message:"Veuillez entrer un fichier !"
+    }),
+    fileType: z.string().min(1,{
+        message:"Veuillez entrer un type de fichier !"
+    })
+})
