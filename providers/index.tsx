eeb2159cbb5b2d7bc4d3c0f804aabd767d9c9dc9
franckniat <niatmanuel2002@gmail.com>
2024-03-29 22:16:27 +0100

    (update) components styles and add toaster on providers

--- providers/index.tsx
+++ providers/index.tsx
@@ -1,9 +1,11 @@
-import React from "react"
-import NextThemesProvider from "./theme-provider"
+import React from "react";
+import NextThemesProvider from "./theme-provider";
+import { Toaster } from "sonner";
 
 export default function Providers({children}:{children:React.ReactNode}){
     return(
         <NextThemesProvider enableSystem defaultTheme="system" attribute="class">
+            <Toaster />
             {children}
         </NextThemesProvider>
     )
