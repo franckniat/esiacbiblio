1eb6c4f2b551c79c446c9953692ba5f105bdbab8
franckniat <niatmanuel2002@gmail.com>
2024-04-05 21:46:24 +0100

    (add) custom breadcrumb

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import { auth, signOut } from '@/auth';
 import { Button } from '@/components/ui/button';
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
+import CustomBreadcrumb from '@/components/ui/custom-breadcrumb';
 
 
 export default async function Dashboard() {
@@ -8,13 +9,11 @@ export default async function Dashboard() {
   return (
     <div className='px-3 py-1'>
       <div className="my-3 flex flex-col gap-3">
-            <Breadcrumb>
-                <BreadcrumbList>
-                    <BreadcrumbItem>
-                        <BreadcrumbLink href="/dashboard">Tableau de bord</BreadcrumbLink>
-                    </BreadcrumbItem>
-                </BreadcrumbList>
-            </Breadcrumb>
+            <CustomBreadcrumb 
+                path={[
+                    {name: 'Tableau de bord', href: '/dashboard'},
+                ]}
+            />
             <div className='space-y-3'>
                 <h1 className="text-2xl tracking-tight font-semibold text-neutral-950 dark:text-white">Tableau de bord</h1>
                 <p className='text-sm leading-6 indent-1'>Hello {session?.user.name}, </p>
