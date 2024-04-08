79b5e97c8c3e9bc1f301168aae5284b784b5710c
franckniat <niatmanuel2002@gmail.com>
2024-04-08 18:18:55 +0100

    (style) handle the loading state of the document page

--- app/documents/loading.tsx
+++ app/documents/loading.tsx
@@ -1,32 +1,10 @@
-
 import Navbar from '@/components/ui/header';
-import { ChevronRight } from 'lucide-react';
 import Footer from '@/components/ui/footer';
-import { getActiveDocuments } from '@/data/documents';
-import DocumentCard from '@/components/document';
-import { getUserById } from '@/data/user';
 import CustomBreadcrumb from "@/components/ui/custom-breadcrumb";
-import { Metadata } from "next";
+import DocumentSkeleton from "@/components/document/document-skeleton";
 
-export async function generateMetadata(
-): Promise<Metadata> {
-    return {
-        title: "Documents - ESIAC BIBLIO",
-        description: "Venez à la découverte d'une multitude de rapports, cours et documents.",
-        
-    }
-}
 
-export default async function Documents() {
-    const allDocuments = await getActiveDocuments();
-    const documentsWithUser = await Promise.all(allDocuments.map(async (document) => {
-        const user = await getUserById(document.userId);
-        return {
-            ...document,
-            user: user
-        };
-    }));
-    const documents = documentsWithUser;
+export default async function LoadingDocuments() {
     return (
         <>
             <Navbar />
@@ -55,18 +33,9 @@ export default async function Documents() {
                 </section>
                 <section
                     className="px-2 md:px-5 mt-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 pb-10">
-                    {documents && documents.map((document, index) => (
-                        <DocumentCard
-                            key={index}
-                            id={document.id}
-                            author={document.user?.name}
-                            title={document.title}
-                            description={document.description}
-                            date={document?.createdAt?.toLocaleDateString("fr-FR", { year: "numeric", month: "long", day: "numeric" })}
-                            sector={document.sector}
-                            fileURL={document.fileURL}
-                        />
-                    ))}
+                    <DocumentSkeleton/>
+                    <DocumentSkeleton/>
+                    <DocumentSkeleton/>
                 </section>
             </main>
             <Footer />
