07057b4e7263c0c035eee31febc46020b175fcc4
franckniat <niatmanuel2002@gmail.com>
2025-01-24 13:33:19 +0100

    style: show scroll-top button only when user scroll to bottom

--- components/scroll-top.tsx
+++ components/scroll-top.tsx
@@ -1,11 +1,23 @@
 "use client";
 import {Button} from "@/components/ui/button";
 import {ChevronUp} from "lucide-react";
+import {useEffect} from "react";
 
 export default function ScrollTop(){
+    useEffect(()=>{
+        const handleScroll = () => {
+            if (window.scrollY > 50) {
+                document.querySelector('.scroll-top')?.classList.add('visible')
+            } else {
+                document.querySelector('.scroll-top')?.classList.remove('visible')
+            }
+        }
+        window.addEventListener('scroll', handleScroll)
+        return () => window.removeEventListener('scroll', handleScroll)
+    })
     return(
         <Button
-            className={"hover:scale-110 transition-transform w-12 h-12 fixed bottom-4 right-4 z-50 will-change-transform rounded-full active:scale-90"}
+            className={"scroll-top hover:scale-110 transition-transform w-12 h-12 fixed bottom-4 right-4 z-50 will-change-transform rounded-full active:scale-90"}
             onClick={()=>{
                 window.scrollTo({top: 0, behavior: 'smooth'});
             }}
