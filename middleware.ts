6de99eb8f150a53e228eaf4cdd707dc86c549977
franckniat <niatmanuel2002@gmail.com>
2025-02-06 22:12:29 +0100

    fix middleware issue

--- middleware.ts
+++ middleware.ts
@@ -4,25 +4,24 @@ import {apiAuthPrefix, authRoutes, DEFAULT_LOGIN_REDIRECT, publicRoutes} from "@
 
 const { auth } = NextAuth(authConfig)
 
-export default auth((req)=>{
-  const {nextUrl}= req;
-  const isLoggedIn = !!req.auth;
-  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
-  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
-  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
-  if(isApiAuthRoute){
-    return null;
-  }
-  if(isAuthRoute){
-    if(isLoggedIn){
-      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+export default auth(async function middleware(req){
+    const { nextUrl } = req;
+    const isLoggedIn = !!req.auth;
+    const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+    const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+    const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+    if (isApiAuthRoute) {
+        return;
     }
-    return null;
-  }
-  if(!isLoggedIn && !isPublicRoute){
-    return Response.redirect(new URL("/auth/login", nextUrl));
-  }
-  return null;
+    if (isAuthRoute) {
+        if (isLoggedIn) {
+            return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+        }
+        return;
+    }
+    if (!isLoggedIn && !isPublicRoute) {
+        return Response.redirect(new URL("/auth/login", nextUrl));
+    }
 })
 
 export const config = {
