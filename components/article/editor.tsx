610b9576a911b120fb07bd88a5053b505d634f5f
franckniat <niatmanuel2002@gmail.com>
2025-02-10 16:55:26 +0100

    update editor style, remove edgestore, handle data saving and loading

--- components/article/editor.tsx
+++ components/article/editor.tsx
@@ -1,46 +1,78 @@
-"use client"; // this registers <Editor> as a Client Component
-import {
-  useCreateBlockNote,
-} from "@blocknote/react";
-import "@blocknote/core/fonts/inter.css";
-import "@blocknote/react/style.css";
-//import { useEdgeStore } from "@/lib/edgestore";
+"use client";
+import React, {useMemo} from "react";
+import {useTheme} from "next-themes";
+import {useEffect, useState} from "react";
+import {Space_Grotesk} from "next/font/google";
 import { BlockNoteView } from "@blocknote/mantine";
+import {Block, BlockNoteEditor, PartialBlock} from "@blocknote/core";
+import "@blocknote/mantine/style.css";
+import "@blocknote/core/fonts/inter.css";
 
-interface EditorProps{
-  onChange: () => void;
+interface EditorProps {
+    onChange: () => void;
+    editor: BlockNoteEditor;
+    defaultValue?: string;
 }
 
-export default function Editor({
-  onChange,
-  ...props
-}:EditorProps) {
+const space_grotesk = Space_Grotesk({subsets: ["latin"]});
 
-  //const {edgestore} = useEdgeStore();
 
-  const handleUpload  = async(file:File)=>{
-    const response = await URL.createObjectURL(file);
-    return response;
-  }
-  /*const handleDelete = async(url:string)=>{
-    await edgestore.publicFiles.delete({
-      url
-    })
-  }*/
-  const editor = useCreateBlockNote({
-    uploadFile: handleUpload,
-  });
+export default function Editor({editor, onChange, defaultValue}: EditorProps) {
+    const { theme } = useTheme();
+    const [isDark, setIsDark] = useState(false);
+    useEffect(() => {
+        if (theme === "dark") {
+            setIsDark(true);
+        } else if (theme === "light") {
+            setIsDark(false);
+        }
+    }, [theme]);
+    async function saveToStorage(jsonBlocks: Block[]) {
+        // Save contents to local storage. You might want to debounce this or replace
+        // with a call to your API / database.
+        localStorage.setItem("editorContent", JSON.stringify(jsonBlocks));
+    }
 
-  return (
-    <BlockNoteView 
-      editor={editor} 
-      onChange={onChange} 
-      className="w-full" 
-      theme={"light"} 
-      title={"Ajouter un article"}
-      lang="fr-FR"
-      sideMenu={true}
-        {...props}
-    />
-  );
+    async function loadFromStorage() {
+        // Gets the previously stored editor contents.
+        const storageString = localStorage.getItem("editorContent");
+        return storageString
+            ? (JSON.parse(storageString) as PartialBlock[])
+            : undefined;
+    }
+
+    const [initialContent, setInitialContent] = useState<
+        PartialBlock[] | undefined | "loading"
+    >("loading");
+
+    useEffect(() => {
+        loadFromStorage().then((content) => {
+            setInitialContent(content);
+        });
+    }, []);
+
+    const editor2 = useMemo(() => {
+        if (initialContent === "loading") {
+            return undefined;
+        }
+        return BlockNoteEditor.create({ initialContent });
+    }, [initialContent]);
+
+    if (editor === undefined) {
+        return "Loading content...";
+    }
+
+    return (
+        <BlockNoteView
+            editor={editor}
+            onChange={onChange}
+            className={`${space_grotesk.style}`}
+            theme={isDark ? "dark" : "light"}
+            lang="fr-FR"
+            emojiPicker={true}
+            defaultValue={defaultValue}
+            formattingToolbar={true}
+            id={"editor"}
+        />
+    );
 }
