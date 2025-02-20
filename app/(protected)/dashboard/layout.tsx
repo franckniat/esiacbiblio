365d47f475843f6e75e72052d8332b584d09bcd6
franckniat <niatmanuel2002@gmail.com>
2025-02-20 12:14:47 +0100

    remove scroll top button on dashboard

--- app/(protected)/dashboard/layout.tsx
+++ app/(protected)/dashboard/layout.tsx
@@ -2,8 +2,7 @@
 import React from "react";
 import { SidebarProvider } from "@/components/ui/sidebar"
 import { AppSidebar } from "@/components/app-sidebar"
-import { cookies } from "next/headers"
-import ScrollTop from "@/components/layouts/scroll-top";
+import { cookies } from "next/headers";
 import DashboardHeader from "@/components/dashboard/dashboard-header";
 
 
@@ -20,7 +19,6 @@ export default async function PublicLayout({
 				<AppSidebar />
 				<main className={"w-full"}>
 					<DashboardHeader />
-					<ScrollTop />
 					<div className={"max-w-[1280px] mx-auto px-4 pt-[75px]"}>{children}</div>
 				</main>
 			</SidebarProvider>
