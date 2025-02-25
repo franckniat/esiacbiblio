07ca2d0d24438913c6d4b17fec2461f92bafd84d
franckniat <niatmanuel2002@gmail.com>
2025-02-25 01:07:57 +0100

    add protedcted routes in the middleware config

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { authConfig } from "@/auth.config";
 import NextAuth from "next-auth";
-import {apiAuthPrefix, authRoutes, DEFAULT_LOGIN_REDIRECT, publicRoutes} from "@/routes"
+import {apiAuthPrefix, authRoutes, DEFAULT_LOGIN_REDIRECT, protectedRoutes} from "@/routes"
 
 const { auth } = NextAuth(authConfig)
 
@@ -8,8 +8,8 @@ export default auth(async function middleware(req){
     const { nextUrl } = req;
     const isLoggedIn = !!req.auth;
     const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
-    const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
     const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+    const isProtectedRoute = protectedRoutes.includes(nextUrl.pathname);
     if (isApiAuthRoute) {
         return;
     }
@@ -19,7 +19,7 @@ export default auth(async function middleware(req){
         }
         return;
     }
-    if (!isLoggedIn && !isPublicRoute) {
+    if (!isLoggedIn && isProtectedRoute) {
         return Response.redirect(new URL("/auth/login", nextUrl));
     }
 })
