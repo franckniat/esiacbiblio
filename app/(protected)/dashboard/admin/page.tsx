4bcba6b229dcde5041213ea631743c48e4c66aca
franckniat <niatmanuel2002@gmail.com>
2025-03-18 00:51:05 +0100

    block users from accessing admin page

--- app/(protected)/dashboard/admin/page.tsx
+++ app/(protected)/dashboard/admin/page.tsx
@@ -1,5 +1,10 @@
 import {redirect} from "next/navigation";
+import {getCurrentUser} from "@/lib/user";
 
-export default function AdminPage() {
+export default async function AdminPage() {
+    const user = await getCurrentUser()
+    if(!user?.role || user.role !== "admin") {
+        redirect("/dashboard")
+    }
     redirect("/dashboard/admin/documents");
 }
