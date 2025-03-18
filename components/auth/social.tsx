d83fe9c456bd5387d086503418a2fc43d46722fc
franckniat <niatmanuel2002@gmail.com>
2025-03-18 21:26:18 +0100

    feat: remove facebook login

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -2,7 +2,7 @@
 
 import { signIn } from "next-auth/react";
 import { Button } from "@/components/ui/button";
-import { Google, Github, Facebook } from "react-bootstrap-icons";
+import { Google, Github } from "react-bootstrap-icons";
 import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 
 export default function SocialButtons() {
@@ -12,14 +12,14 @@ export default function SocialButtons() {
 		});
 	};
 	return (
-		<div className="max-w-md mx-auto grid grid-cols-3 gap-2">
+		<div className="max-w-md mx-auto grid grid-cols-2 gap-2">
 			<Button
 				variant={"outline"}
 				className="w-full"
 				title="Se connecter avec Google"
 				onClick={() => onClick("google")}
 			>
-				<Google size={18} />
+				<Google size={18} /> Google
 			</Button>
 			<Button
 				variant={"outline"}
@@ -27,15 +27,7 @@ export default function SocialButtons() {
 				title="Se connecter avec Github"
 				onClick={() => onClick("github")}
 			>
-				<Github size={18} />
-			</Button>
-			<Button
-				variant={"outline"}
-				className="w-full"
-				title="Se connecter avec Facebook"
-				onClick={() => onClick("facebook")}
-			>
-				<Facebook size={18} />
+				<Github size={18} /> Github
 			</Button>
 		</div>
 	);
